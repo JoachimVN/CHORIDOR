597a2916d6e9d919f6bc77cce30106ba47320019
Joachim Valdersnes Nilsen <joachim.v.nilsen@gmail.com>
2026-06-05 00:48:53 +0200

    fix: SonarQube issues

--- src/main/java/io/github/joachimvn/App.java
+++ src/main/java/io/github/joachimvn/App.java
@@ -3,9 +3,11 @@ package io.github.joachimvn;
 import java.util.logging.Logger;
 
 public class App {
+    private App() {}
+
     private static final Logger LOGGER = Logger.getLogger(App.class.getName());
 
-    public static void main() {
+    public static void run() {
         LOGGER.info("Hello World!");
     }
 }
